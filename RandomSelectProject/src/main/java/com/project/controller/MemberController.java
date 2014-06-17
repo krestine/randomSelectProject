fb5aade748f9b536c93a463c5a84d49303894116
echigoen <echigoen@naver.com>
2014-06-17 18:21:37 +0900

    ff

--- RandomSelectProject/src/main/java/com/project/controller/MemberController.java
+++ RandomSelectProject/src/main/java/com/project/controller/MemberController.java
@@ -1,12 +1,54 @@
 package com.project.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.project.service.MemberService;
+
+@Controller
 public class MemberController {
-	//회원가입
-	//로그인
-	//아이디찾기
-	//비밀번호찾기
-	//내정보
-	//현재비밀번호확인
-	//정보수정
-	//탈퇴
+
+	@Autowired
+	private MemberService memberService;
+
+	// 회원가입
+
+	
+	
+	
+	// 로그인
+
+	
+	
+	
+	// 아이디찾기
+
+	
+	
+	
+	// 비밀번호찾기
+
+	
+	
+	
+	// 내정보
+
+	
+	
+	
+	// 현재비밀번호확인
+
+	
+	
+	
+	// 정보수정
+
+	
+	
+	
+	// 탈퇴
+
+	
+	
+	
 }
