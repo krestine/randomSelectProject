533dc8181c16b8cd209c8fd5e6c12142a76758df
echigoen <echigoen@naver.com>
2014-06-17 20:29:10 +0900

    ee

--- RandomSelectProject/src/main/java/com/project/controller/CommunityController.java
+++ RandomSelectProject/src/main/java/com/project/controller/CommunityController.java
@@ -1,5 +1,25 @@
 package com.project.controller;
 
-public class CommunityController {
+import com.project.service.EvaluateService;
+import com.project.service.MateService;
+import com.project.service.MenuService;
+import com.project.service.RestntService;
 
+public class CommunityController {
+	// 친구 리스트
+	private MateService mateService;
+	private RestntService restntService;
+	private EvaluateService evaluateService;
+	private MenuService menuService;
+	
+	
+	// 친구 상세정보
+	
+	
+	// 식당 리스트
+	
+	
+	// 식당 상세정보
+	
+	
 }
