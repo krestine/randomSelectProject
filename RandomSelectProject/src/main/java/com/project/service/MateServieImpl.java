1c8587bd1fed98bbc4bea9c2412a59f133c809b0
aseurai <ttubhk@gmail.com>
2014-06-18 15:19:43 +0900

    dd

--- RandomSelectProject/src/main/java/com/project/service/MateServieImpl.java
+++ RandomSelectProject/src/main/java/com/project/service/MateServieImpl.java
@@ -8,13 +8,14 @@ import com.project.domain.MateDTO;
 public class MateServieImpl implements MateService {
 	private MateMapper mateMapper;
 	
-	
+	// 친구 리스트
 	@Override
 	public List<MateDTO> getMateIdsByMemId(MateDTO mateDto) {
 		
 		return mateMapper.getMateIdsByMemId(mateDto);
 	}
-
+	
+	// 친구 아이디
 	@Override
 	public MateDTO getMateIdByMemId(MateDTO mateDto) {
 		
