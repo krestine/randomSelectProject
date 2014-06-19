68f7251eebef4a7040c249868847b52643a87475
HSeoYeon <sweetmintcho@naver.com>
2014-06-19 09:59:45 +0900

    df

--- RandomSelectProject/src/main/java/com/project/dao/EvaluateMapper.java
+++ RandomSelectProject/src/main/java/com/project/dao/EvaluateMapper.java
@@ -5,6 +5,20 @@ import java.util.List;
 import com.project.domain.EvaluateDTO;
 
 public interface EvaluateMapper {
-	//관리자 : 유저의 평가 목록 열람
-	List<EvaluateDTO> getEvaluateListByMemId(String memId); 
+	// 관리자 : 유저의 평가 목록 열람
+	List<EvaluateDTO> getEvaluateListByMemId(String memId);
+
+	// 평가한 방문 리스트
+	EvaluateDTO getEvaluateListByMemId(EvaluateDTO evaluateDto);
+
+	// 평가한 방문리스트 수정
+	EvaluateDTO setEvaluationInfoByEvaluationTerms(EvaluateDTO evaluateDto);
+
+	// 평가안한 방문리스트
+	EvaluateDTO getnEvaluateListByMemId(EvaluateDTO evaluateDto);
+
+	/*
+	 * // 식당평가 안한 점수 입력 EvaluateDTO
+	 * setEvaluationInfoByEvaluationTerms(EvaluateDTO evaluateDto); //
+	 */
 }
