67d4956f510872324732533c45bfdb627f19a7e7
krestine <krestine@nate.com>
2014-06-19 11:10:37 +0900

    1

--- RandomSelectProject/src/main/java/com/project/controller/RandomSelectController.java
+++ RandomSelectProject/src/main/java/com/project/controller/RandomSelectController.java
@@ -1,5 +1,37 @@
 package com.project.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.project.domain.MemberDTO;
+import com.project.domain.RestntDTO;
+import com.project.service.RestntService;
+
+@Controller
 public class RandomSelectController {
 	
+	@Autowired
+	private RestntService restntService;
+	
+	@RequestMapping("/selectResult.do")
+	public String randomSelectMain(Model model, HttpServletRequest request){
+		HttpSession session = request.getSession();
+		MemberDTO memberDto = (MemberDTO) session.getAttribute("memberDto");
+		if(memberDto==null){
+			model.addAttribute("loginMode", "0");
+		}
+		else{
+			model.addAttribute("loginMode", memberDto.getMemGrade());
+		}
+		List<RestntDTO> restntList = restntService.getRestntList();
+		model.addAttribute("restntList", restntList);
+		return "selectResult";
+	}
 }
