2ab7b12db11b2d53ee1e72ba2b95cc984938f685
SungHoiKoo <apvltmxh12@naver.com>
2014-06-24 17:10:56 +0900

    에러 대충 잡았다

--- RandomSelectProject/src/main/java/com/project/service/MateServieImpl.java
+++ RandomSelectProject/src/main/java/com/project/service/MateServieImpl.java
@@ -3,10 +3,12 @@ package com.project.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.project.dao.MateMapper;
 import com.project.domain.MateDTO;
 
+@Service
 public class MateServieImpl implements MateService {
 	
 	@Autowired
