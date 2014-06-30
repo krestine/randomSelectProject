54143c5ea95aa2728cef375c89fe9678247995fc
HSeoYeon <sweetmintcho@naver.com>
2014-06-30 21:47:37 +0900

    dfsdfsfd

--- RandomSelectProject/src/main/java/com/project/controller/EvaluateController.java
+++ RandomSelectProject/src/main/java/com/project/controller/EvaluateController.java
@@ -1,9 +1,9 @@
-
 package com.project.controller;
 
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,96 +17,113 @@ import com.project.domain.MemberDTO;
 import com.project.domain.MenuDTO;
 import com.project.domain.RestntDTO;
 import com.project.service.EvaluateService;
+import com.project.service.MemberService;
+import com.project.service.RestntService;
 
 @Controller
 public class EvaluateController {
 
 	@Autowired
 	private EvaluateService evaluateService;
-	
-	// 회원 리스트에서 아이디 클릭 -> 회원의 평가 정보 리스트 표시 페이지
-			@RequestMapping(value = "/memberEvaluateListProc.do", method = RequestMethod.POST)
-			String memberEvaluateListProc(Model model, String memId, List<EvaluateDTO> RestntEvaluates) {
-				RestntEvaluates = evaluateService.getEvaluateListByMemId(memId);
-				model.addAttribute("RestntEvaluates", RestntEvaluates);
-				return "admin/memberEvaluateListAdmin";
-			}
 
-		/*	// 회원 리스트에서 등급, 블랙 수정내용 입력후 수정 버튼 클릭
-			@RequestMapping(value = "/setMemberInfoProc.do", method = RequestMethod.POST)
-			String setMemberInfo(MemberDTO memberDto, Model model) {
-				System.out.println("수정 버튼 클릭");
-				if (memberDto.getBlack() == null) {
-					memberDto.setBlack("0");
-				}
-				System.out.println(memberDto);
-				String memId = memberDto.getMemId();
-				memberService.setMemberInfo(memberDto);
-				members = memberService.getMemberListById(memId);
-				model.addAttribute("members", members);
-				return "admin/memberList";
-			}
-		*/
+	/*
+	 * @Autowired
+	 * 
+	 * /* // 회원 : 식당 리스트
+	 * 
+	 * @RequestMapping(value = "/evaluateListProc.do", method =
+	 * RequestMethod.POST) public String restntListProc(Model model, RestntDTO
+	 * restntDto) { List<RestntDTO> restnts = restntService.getRestntList();
+	 * model.addAttribute("restnts", restnts);
+	 * System.out.println("restntListProc()"); return "evaluate/evaluateList"; }
+	 */
+	// 평가 페이지
+	@RequestMapping(value = "/evaluatemain.do", method = RequestMethod.POST)
+	public String evaluatemain(Model model, String memId,
+			HttpServletRequest request) {
+		System.out.println("evaluatemain()");
+		// 세션에서 로그인 정보 가져옴
+		MemberDTO loginUser = (MemberDTO) request.getSession().getAttribute(
+				"loginUser");
+		// 로그인 정보의 아이디를 패러미터로 세팅
 
-	
-	
+		List<EvaluateDTO> evaluates = evaluateService
+				.getEvaluateListByMemId(loginUser.getMemId());
+		model.addAttribute("evaluates", evaluates);
+		return "evaluate/evaluate";
+	}
+
 	// 식당평가한 목록
-	
-		@RequestMapping(value = "/evaluateListForm.do", method = RequestMethod.POST)
-		public String EvaluateListForm(Model model, String box, String memId) {
-			System.out.println("evaluateListForm()");
-		
-			return "evaluate/edit";
-		}
-	
-		
-		//평가 수정editOk
-		@RequestMapping(value="editOk.do", method = RequestMethod.POST)
-		public String editOk(Model model, HttpServletRequest request) {
-		//저장해줘..(쿼리문으로 xml에 쓰면돼)
-			
-			return "evaluate/editOk";
-			}
-		
-	@RequestMapping(value="/evaluateListProc.do", method = RequestMethod.POST)
-	public String evaluateListProc(Model model, String memId, HttpServletRequest request) {
+
+	@RequestMapping(value = "/evaluateListForm.do", method = RequestMethod.POST)
+	public String EvaluateListForm(HttpServletRequest request, Model model,
+			String memId) {
+		System.out.println("evaluateListForm()");
+		MemberDTO loginUser = (MemberDTO) request.getSession().getAttribute(
+				"loginUser");
+		System.out.println(loginUser.toString());
+		memId = loginUser.getMemId();
+		System.out.println(memId);
+		model.addAttribute("getEvaluateListByMemId",
+				evaluateService.getEvaluateListByMemId(memId));
+		return "evaluate/evaluateList";
+	}
+
+	// 평가 수정editOk
+	@RequestMapping(value = "edit.do", method = RequestMethod.POST)
+	public String edit(Model model, HttpServletRequest request) {
+		// 저장해줘..(쿼리문으로 xml에 쓰면돼)
+
+		return "evaluate/edit";
+	}
+
+	// 평가 수정editOk
+	@RequestMapping(value = "editOk.do", method = RequestMethod.POST)
+	public String editOk(Model model, HttpServletRequest request) {
+		// 저장해줘..(쿼리문으로 xml에 쓰면돼)
+
+		return "evaluate/editOk";
+	}
+
+	@RequestMapping(value = "/evaluateListProc.do", method = RequestMethod.POST)
+	public String evaluateListProc(Model model, String memId,
+			HttpServletRequest request) {
 		System.out.println("evaluateListProc()");
 
-		
-		MemberDTO loginUser = (MemberDTO) request.getSession().getAttribute("loginUser");
+		MemberDTO loginUser = (MemberDTO) request.getSession().getAttribute(
+				"loginUser");
 		try {
 			memId = loginUser.getMemId();
 		} catch (Exception e) {
-			model.addAttribute("errorMessage",
-					"로그인 해 주세요!");
+			model.addAttribute("errorMessage", "로그인 해 주세요!");
 			return "setting/error";
 		}
-		
+
 		System.out.println("평가 리스트 실행");
 		try {
-			List<EvaluateDTO> evaluates = evaluateService.getEvaluateListByMemId(memId);
+			List<EvaluateDTO> evaluates = evaluateService
+					.getEvaluateListByMemId(memId);
 			model.addAttribute("evaluates", evaluates);
 			return "evaluate/evaluateList";
-		
+
 		} catch (Exception e) {
-			model.addAttribute("errorMessage", "데이터 베이스 오류가 발생했습니다<br> 잠시 후에 다시 시도 해주세요.");
+			model.addAttribute("errorMessage",
+					"데이터 베이스 오류가 발생했습니다<br> 잠시 후에 다시 시도 해주세요.");
 		}
 		return "setting/error";
-		
 
-
-		
 	}
 
 	// 식당 평가 수정
 	@RequestMapping(value = "/updateEvaluateListProc.do", method = RequestMethod.POST)
 	public String updateEvaluateListProc(Model model, EvaluateDTO evaluateDto) {
 		evaluateService.setEvaluateInfoByEvaluateTerms(evaluateDto);
-		EvaluateDTO evaluate=(EvaluateDTO) evaluateService.getEvaluateListByMemId(evaluateDto.getMemId());
+		EvaluateDTO evaluate = (EvaluateDTO) evaluateService
+				.getEvaluateListByMemId(evaluateDto.getMemId());
 		model.addAttribute("evaluate", evaluate);
 		return "evaluateListForm.do";
 	}
-	
+
 	// 평가 안한 식당목록
 	@RequestMapping(value = "/nEvaluateListForm.do", method = RequestMethod.POST)
 	public String nEvaluateListForm(Model model, String memId) {
@@ -123,4 +140,3 @@ public class EvaluateController {
 		return "evaluate/nEvaluateListForm.do";
 	}
 }
-
